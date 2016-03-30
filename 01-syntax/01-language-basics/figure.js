d1c2c57c7196b1bbf08bf6bbf83e81d6b5092b84
bazzika <daryabelob@gmail.com>
2016-03-30 13:12:04 +0300

    - add figure "high triangle"

--- 01-syntax/01-language-basics/figure.js
+++ 01-syntax/01-language-basics/figure.js
@@ -1,3 +1,38 @@
-/**
- * Created by daria.bieloborodova on 3/30/2016.
- */
+var pyramid = "";
+d = 50;
+for (var j = 0; j <= d ; j++ ) {
+  for (var i = 0; i <= d/2; i++){
+    if ((j > (d/2-i))&&(j < (d/2+i))){
+      pyramid += "#";
+    } else {
+      pyramid += " ";
+    }
+    /*if ((i > (d/2-j))&&(i < (d/2+j))){
+      pyramid += "#";
+    } else {
+      pyramid += " ";
+    }*/
+  }
+  for (var i = 0; i <= d/2; i++){
+    if ((i > (d / 2 + j)) && (i < (d / 2 - j))) {
+      pyramid += "#";
+    } else {
+      pyramid += " ";
+    }
+  }
+    console.log(pyramid);
+  pyramid = "";
+}
+/*for (var j = 0; j <= d ; j++ ) {
+  for (var i = 0; i <= d; i++){
+    if ((j > (d/2-i))&&(j < (d/2+i))){
+      pyramid += "#";
+    } else {
+      pyramid += " ";
+    }
+  }
+  console.log(pyramid);
+  pyramid = "";
+} */
+
+
